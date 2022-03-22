9c8565859e0b37a9699e4fc52e522763140a4ab6
Megan Kuo <megan.kuo@live.com>
2022-03-22 11:38:14 -0700

    avg

--- results.php
+++ results.php
@@ -11,22 +11,23 @@
 
 <body>
 
-<?php
+    <?php
 
-$_SESSION['played_today'] = true;
-$timeResult = $_POST['resultTime'];
-$goal = $_POST['goal'];
-if(isset($_POST['resultTime'])){
-echo "<h2>$goal clicks done in $timeResult seconds!!!!!!!!</h2>";
-echo "<div class='container'>Take a screenshot and show your friends!</div>";
-echo "<a href='index.php'><button>Try again!</button></a>";
-} else {
-    header("Location: index.php");
-    die();
-}
+    $_SESSION['played_today'] = true;
+    $timeResult = $_POST['resultTime'];
+    $goal = $_POST['goal'];
+    $avg = $goal / $timeResult;
+    if (isset($_POST['resultTime'])) {
+        echo "<h2>$goal clicks done in $timeResult seconds!!!!!!!!</h2>";
+        echo "<div class='container'>Average clicks per second: $avg</div>";
+        echo "<div class='container'>Take a screenshot and show your friends!</div>";
+        echo "<a href='index.php'><button>Try again!</button></a>";
+    } else {
+        header("Location: index.php");
+        die();
+    }
 
-?>
+    ?>
 </body>
 
 </html>
-
